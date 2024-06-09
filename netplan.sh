a79965fbfc27dd89f9e0c6f59ad387a7f98ae051
masfer <ferhadmaulidi@gmail.com>
2024-06-09 16:53:15 +0700

    update script

---
+++ netplan.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Creates a backup
+cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bak_`date +%Y%m%d%H%M`
+# Retrieves the NIC information
+nic=`ifconfig | awk 'NR==1{print $1}'`
+# Ask for input on network configuration
+read -p "Masukan static IP Server: " staticip
+read -p "Masukan IP of gateway: " gatewayip
+read -p "Masukan IP of dns server (seperated by a coma if more than one): " nameserversip
+echo
+cat > /etc/netplan/00-installer-config.yaml <<EOF
+
+network:
+  ethernets:
+    $nic
+      addresses:
+        - $staticip/24
+      gateway4: $gatewayip
+      nameservers:
+        addresses:
+          - $nameserversip
+        search: []
+  version: 2
+          
+EOF
+sudo netplan apply
+echo "==========================="
+echo
+
---
+++ run-after-clone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# generate machine id
+systemd-machine-id-setup
+# remove ssh host keys
+sudo rm -f /etc/ssh/ssh_host_*
+# generate ssh key
+ssh-keygen -A && service ssh --full-restart
+sleep 1
+# generate password root
+passwd root
+sleep 1
+#Set IP SERVER
+cd "$(pwd)"; sh netplan.sh
+# truncate the machine-id file
+sudo truncate -s 0 /etc/machine-id
+
